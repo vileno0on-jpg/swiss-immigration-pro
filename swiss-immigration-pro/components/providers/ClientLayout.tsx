cb33149f4ed533de533ff388364e3053ba7f4684
vileno0on-jpg <vileno0on@gmail.com>
2025-12-14 18:01:26 +0100

    Remove duplicate UrgencyBanner header

--- swiss-immigration-pro/components/providers/ClientLayout.tsx
+++ swiss-immigration-pro/components/providers/ClientLayout.tsx
@@ -7,7 +7,6 @@ import { InitialQuizGate } from '@/components/quiz/InitialQuizGate'
 import { TranslationLoader } from '@/components/TranslationLoader'
 import ScrollToTop from '@/components/layout/ScrollToTop'
 import { ToastProvider } from '@/components/providers/ToastProvider'
-import UrgencyBanner from '@/components/marketing/UrgencyBanner'
 import dynamic from 'next/dynamic'
 
 // Dynamically import FloatingChatWidget to avoid SSR issues - completely separate component
@@ -31,7 +30,6 @@ export function ClientLayout({ children }: { children: React.ReactNode }) {
     <SessionProvider>
       <ToastProvider>
         <TranslationLoader />
-        <UrgencyBanner />
         <InitialQuizGate />
         <main id="main-content" className="flex-1 transition-all duration-300 ease-out">
           {children}
